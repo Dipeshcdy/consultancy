73dabf4d9e841406f6c1fc5ec38d4cfbc7d14264
Dipeshcdy <techdipesh36@gmail.com>
2024-02-04 22:59:36 +0545

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import { useState } from 'react'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import Home from './components/Home.jsx'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import MessageFromCeo from "./components/MessageFronCeo.jsx";
 import './App.css'
 
 
 function App() {
+  const location = useLocation();
   return (
 
     <>
       <Router>
       <Navbar />
-        <Routes>
+        <Routes key={location.pathname} location={location} >
           <Route exact path='/' element={
             <>
               <section className='mt-[7.8rem]'>
